4b3a2bc3bf0fb918de55448a574af12200cdde35
annalise.t <annalise.t@protonmail.com>
2021-04-02 23:11:30 +1100

    stuff

--- flask_practice.py
+++ flask_practice.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, request
+import random
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'this is the homepage'
+@app.route('/joke/<int:numb>')
+def random_joke(numb):
+    if numb == 1:
+        return "Why did Steve fall off the tractor ............ because Steve was a strawberry"
+    elif numb == 2:
+        print("My wife told me to stop acting like a flamingo")
+        print("So I had to put my foot down")
+    elif numb == 3:
+        print("Did you hear about the mathematician")
+        print("He’ll stop at nothing to avoid them.")
+    elif numb == 4:
+        print("Did you hear about the restaurant called Karma?")
+        print("There is no menu, you get what you deserve")
+    elif numb == 5:
+        print("Did you hear about the actor who fell through the floorboards?")
+        print("He was probably just going through a stage.")
+    return 'hi'
 
+@app.route("/profile/name")
+def profile(name):
+    return render_template("profile.html", name=name)
+
+    
 if __name__ == '__main__':
     app.run(debug=True)
