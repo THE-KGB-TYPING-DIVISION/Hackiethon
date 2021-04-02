76ec2e8f67255e3555a2d2c1fd1e3b1bcc622590
“Benjamin <benjamin.thomas160@gmail.com>
2021-04-02 22:46:55 +1100

    new

--- randomjoke.py
+++ randomjoke.py
@@ -1,7 +1,21 @@
+from flask import Flask
+
 import random
+
+
 numb = random.randint(1,5)
 if numb == 1:
     print("Why did Steve fall off the tractor")
     print("because Steve was a strawberry")
-else :
-    print("ha")
+elif numb == 2:
+    print("My wife told me to stop acting like a flamingo")
+    print("So I had to put my foot down")
+elif numb == 3:
+    print("Did you hear about the mathematician")
+    print("He’ll stop at nothing to avoid them.")
+elif numb == 4:
+    print("Did you hear about the restaurant called Karma?")
+    print("There is no menu, you get what you deserve")
+elif numb == 5:
+    print("Did you hear about the actor who fell through the floorboards?")
+    print("He was probably just going through a stage.")
