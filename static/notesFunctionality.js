bb6c23d1deb1ce214f8fd011b8a3d28ab39b729a
annalise.t <annalise.t@protonmail.com>
2021-04-03 23:48:50 +1100

    works somehow

--- static/notesFunctionality.js
+++ static/notesFunctionality.js
@@ -1,14 +1,14 @@
 window.onload = function() {
-    document.getElementById('menulink').onclick = function() {
+    /*document.getElementById('menulink').onclick = function() {
         var menu = document.getElementById('menu');
         if(menu.className != 'shownmenu') {
             menu.className = 'shownmenu';
         }
         else {
             menu.className = 'hiddenmenu';
-        }
+        }*/
     }
-}
+
 function check_web_storage_support() {
     if(typeof(Storage) !== "undefined") {
         return(true);
@@ -18,16 +18,16 @@ function check_web_storage_support() {
         return(false);
     }
 }
-function display_saved_note() {
+window.onload = function display_saved_note() {
     if(localStorage.getItem('note') !== null) {
         result = localStorage.getItem('note');
-        console.log(localStorage.getItem('note'));
     }
     if(result === null) {
         result = "No note saved";
     }
     document.getElementById('area').value = result;
 }
+display_saved_note();
 function save() {
     if(check_web_storage_support() == true) {
         var area = document.getElementById("area");
