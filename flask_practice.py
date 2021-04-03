130b136e981c7a4d34ac4f3838fc2d93d1530460
“Benjamin <benjamin.thomas160@gmail.com>
2021-04-03 20:15:36 +1100

    HAHA

--- flask_practice.py
+++ flask_practice.py
@@ -6,28 +6,6 @@ from os import path
 
 app = Flask(__name__)
 
-
-@app.route('/home/')
-def home():
-    joke = 'rand'
-    numb = random.randint(1,5)
-    if numb == 1:
-        joke = "Why did Steve fall off the tractor? Because Steve was a strawberry"
-    elif numb == 2:
-        joke = "My wife told me to stop acting like a flamingo. So I had to put my foot down"
-    elif numb == 3:
-        joke = "Did you hear about the mathematician who was afraid of negative numbers? He’ll stop at nothing to avoid them."
-    elif numb == 4:
-        joke = "Did you hear about the restaurant called Karma? There is no menu, you get what you deserve"
-    elif numb == 5:
-        joke = "Did you hear about the actor who fell through the floorboards? He was probably just going through a stage."
-    return render_template('homepage.html', joke=joke)
-
-@app.route('/profile/<name>')
-def profile(name):
-    joke = 'maybe'
-    return render_template("profile.html", name=name, joke=joke)
-
 @app.route('/homepage/rick_astley')
 def ricky():
     return render_template('rick_astley.html')
