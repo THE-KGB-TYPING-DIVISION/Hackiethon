992aa9cc2283706fd11c861045818b4d8cda4756
“Benjamin <benjamin.thomas160@gmail.com>
2021-04-03 21:01:35 +1100

    sadfkjn

--- flask_practice.py
+++ flask_practice.py
@@ -12,7 +12,7 @@ def ricky():
 
 @app.route('/homepage/')
 def homepage():
-    joke = 'rand'
+    """joke = 'rand'
     numb = random.randint(1,5)
     if numb == 1:
         joke = "Why did Steve fall off the tractor? Because Steve was a strawberry"
@@ -23,8 +23,8 @@ def homepage():
     elif numb == 4:
         joke = "Did you hear about the restaurant called Karma? There is no menu, you get what you deserve"
     elif numb == 5:
-        joke = "Did you hear about the actor who fell through the floorboards? He was probably just going through a stage."
-    return render_template('homepagePractice.html', joke=joke)
+        joke = "Did you hear about the actor who fell through the floorboards? He was probably just going through a stage."""
+    return render_template('homepagePractice.html')
 
 @app.route('/homepage/about')
 def about():
