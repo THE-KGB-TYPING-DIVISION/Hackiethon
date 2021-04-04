ce57ffbbdd1cf1a1dca82b678880fb8a3d7873b1
“Benjamin <benjamin.thomas160@gmail.com>
2021-04-04 21:01:48 +1000

    asdf

--- static/notesFunctionality.js
+++ static/notesFunctionality.js
@@ -1,17 +1,8 @@
 window.onload = function() {
-        document.getElementById('area').value=localStorage.getItem('note')
-    /*document.getElementById('menulink').onclick = function() {
-        var menu = document.getElementById('menu');
-        if(menu.className != 'shownmenu') {
-            menu.className = 'shownmenu';
-        }
-        else {
-            menu.className = 'hiddenmenu';
-        }*/
-        var stor = 'stuff';
-        document.getElementById('area').value=localStorage.getItem(stor)
+        document.getElementById('area').value=localStorage.getItem(stor);
+    
     }
-var stor = 'stuff';
+
 function check_web_storage_support() {
     if(typeof(Storage) !== "undefined") {
         return(true);
@@ -32,19 +23,25 @@ function display_saved_note() {
 }
 display_saved_note();
 function save() {
-    var stor = 'stuff';
+    
+    var stor = 1;
     if(check_web_storage_support() == true) {
         var area = document.getElementById("area");
         if(area.value != '') {
-            localStorage.setItem(stor, area.value);
+            localStorage.setItem(stor + 1, area.value);
         }
         else {
             alert("Nothing to save");
         }
+
+    }
+    console.log(localStorage.length);
+    for (var i = 0; i < localStorage.length; i++) {
+        console.log(localStorage.getItem(localStorage.key(i)));
     }
 }
+
 function clear() {
-    var stor = 'stuff';
     document.getElementById('area').value = "";
     localStorage.setItem(stor, area.value);
 }
